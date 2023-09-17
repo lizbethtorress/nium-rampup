a358e7b3b6205c2bb0775f7aaa0e90c2f2f556a5
Andrew Lee <namwook0921@gmail.com>
2023-09-17 13:05:29 -0700

    .

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image'
 import {useState} from 'react'
 import { Button } from '@chakra-ui/react';
+import { useFormik } from 'formik'
 
 export default function Home() {
   let count = 0;
@@ -11,6 +12,32 @@ export default function Home() {
     // TODO: update count correctly using state
     count += 1;
   }
+  const SignupForm = () => {
+    // Pass the useFormik() hook initial form values and a submit function that will
+    // be called when the form is submitted
+    const formik = useFormik({
+      initialValues: {
+        email: '',
+      },
+      onSubmit: values => {
+        alert(JSON.stringify(values, null, 2));
+      },
+    });
+    return (
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+  
+        <button type="submit">Submit</button>
+      </form>
+    );
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -35,7 +62,7 @@ export default function Home() {
       Hopefully it's not an issue with Nium's components, but that's why we specified color with 
       both class names and the Chakra UI colorScheme prop
       */}
-      <Button className="bg-sky-600" colorScheme='blue' onClick={handleClick}>Increment</Button>
+      <Button className="bg-sky-600" colorScheme='blue' onClick={SignupForm}>Insert</Button>
       <div>
         Hello world
       </div>
